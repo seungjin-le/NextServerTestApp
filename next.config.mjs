0efc6ef0c1f949a0bcce574b636560f35a1b6f73
seungjin-le <dltmdwls154@gmail.com>
2024-09-30 17:58:11 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  proxy: {
+    "/api": {  
+      target: process.env.NEXT_PUBLIC_URL,
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*/",
+        destination: `${process.env.NEXT_PUBLIC_URL}/:path*/`
+      },
+    ];
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
--- src/middleware.js
+++ src/middleware.js
@@ -7,7 +7,7 @@ export async function middleware(request) {
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
+
 export const config = {
   matcher: "/",
 };
