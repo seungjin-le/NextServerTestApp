06cd7f8dcc542532fd14cf8dca05ddabfee81d36
Lee Seung Jin <dltmdwls154@gmail.com>
2024-10-02 23:50:15 +0900

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  proxy: {
-    "/api": {  
-      target: process.env.NEXT_PUBLIC_URL,
-    },
-  },
   async rewrites() {
+    // Proxy
     return [
       {
         source: "/api/:path*/",
-        destination: `${process.env.NEXT_PUBLIC_URL}/:path*/`
+        destination: `${process.env.NEXT_PUBLIC_URL}/:path*/`,
       },
     ];
+  },
+  env: {
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   },
   trailingSlash: true,
 };
