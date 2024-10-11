da719a754a871611ee9129479df1844a5e3d7cf3
Lee Seung Jin <dltmdwls154@gmail.com>
2024-10-11 23:54:12 +0900

    Test

--- src/app/list/layout.jsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="flex flex-col bg-red-500">
-      <div>children Layout</div>
-      {children}
-    </div>
-  );
-}
--- src/app/list/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Page() {
-  return (
-    <div className="bg-red-500">
-      <Index />
-    </div>
-  );
-}
