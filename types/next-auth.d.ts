6b57f7384f116f3b4638517669c542363995a1a1
Lee Seung Jin <dltmdwls154@gmail.com>
2025-01-03 23:05:57 +0900

    Test

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,11 +1,18 @@
-import { DefaultSession } from "next-auth";
-import NextAuth from "next-auth/next";
+import { DefaultSession } from 'next-auth'
+import NextAuth from 'next-auth/next'
 
-declare module "next-auth" {
+interface Session {
+  user: {
+    username?: string | undefined | null
+    uid?: string | undefined | null
+  } & DefaultSession['user']
+}
+
+declare module 'next-auth' {
   interface Session {
     user: {
-      username?: string | undefined | null;
-      uid?: string | undefined | null;
-    } & DefaultSession["user"];
+      username?: string | undefined | null
+      uid?: string | undefined | null
+    } & DefaultSession['user']
   }
 }
